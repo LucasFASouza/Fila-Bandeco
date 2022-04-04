b91ca09f3bec94c501ec20e727e261413c2eb50b
Lucas Souza <outrolucas2002@gmail.com>
2022-04-03 23:00:28 -0300

    add meta google

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,17 @@ import "../globals.css";
 import type { AppProps } from "next/app";
 import AuthProvider from "../components/authProvider";
 import axios from "axios";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
   return (
     <AuthProvider>
+      <Head key="app">
+        <meta
+          name="google-site-verification"
+          content="41JnQVpbFAf9rGtnptrMLZEEkXDQEiPM7vA4jJgA-Jg"
+        />
+        </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
